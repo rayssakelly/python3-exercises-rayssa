d155cae1058962e2e152c2f14204436c0ba15113
Rayssa Kelly <kellyrayssa80@gmail.com>
2022-08-25 16:54:48 -0300

    aula 08

---
+++ ex021-tocando-uma-musica.py
@@ -0,0 +1,10 @@
+print('Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.')
+#import pygame
+#pygame.init()
+#pygame.mixer.music.load('musica-python.mp3')
+#pygame.mixer.music.play()
+#pygame.event.wait()
+
+import playsound
+from playsound import playsound
+playsound('C:\\Users\\Tomtom\\Desktop\\python rayssa\\exercicios-python\\python3-exercises-rayssa\\music.wav')
