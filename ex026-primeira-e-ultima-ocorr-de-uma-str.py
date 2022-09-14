65c59a05d56dc045cb6873a7bfa8023ecbe228c9
Rayssa Kelly <kellyrayssa80@gmail.com>
2022-09-14 15:40:36 -0300

    Aula 09

--- ex026-primeira-e-ultima-ocorr-de-uma-str.py
+++ ex026-primeira-e-ultima-ocorr-de-uma-str.py
@@ -1,1 +1,8 @@
-print('Exercício Python 26: Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.')
+print('Exercício Python 26: Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece '
+      'a letra “A”, em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.')
+letA = str(input('Digite uma frase:')).lower().strip()
+print('ANALISANDO...')
+print('Na sua frase, a letra A aparece {} vezes.'.format(letA.count('a')))
+print('A letra A aparece em sua frase primeiramente na {}º posição'.format(letA.find('a')+1)) #o +1, é para que a primeira posiçaõ seja 1, não zero como sabemos.#
+print('A letra A aparece em sua frase pela última vez na {}º posição'.format(letA.rfind('a')+1))
+
--- ex027-primeiro-e-ultimo-nome-de-uma-pessoa.py
+++ ex027-primeiro-e-ultimo-nome-de-uma-pessoa.py
@@ -1,1 +1,6 @@
-print('Exercício Python 27: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente')
+print('Exercício Python 27: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o '
+      'primeiro e o último nome separadamente')
+nome = str(input('Digite seu nome completo:')).strip()
+n = nome.split()
+print('Seu primeiro nome é:{}'.format(n[0].capitalize()))
+print('Seu ultimo nome é: {}'.format(n[len(n)-1].capitalize()))
